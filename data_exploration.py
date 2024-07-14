24540bf5721f50632c5854a8c8572f2435092677
surfboyss <116806479+surfboyss@users.noreply.github.com>
2024-07-14 21:27:57 +0200

    Update data_exploration.py

--- data_exploration.py
+++ data_exploration.py
@@ -45,13 +45,4 @@ sns.heatmap(corrmat, vmax = .8, square = True)
 plt.show()
 
 
-#dividing the X and the Y from the dataset
-X=data.drop([‘Class’], axis=1)
-Y=data[“Class”]
-print(X.shape)
-print(Y.shape)
-#getting just the values for the sake of processing (its a numpy array with no columns)
-X_data=X.values
-Y_data=Y.values
-
 
